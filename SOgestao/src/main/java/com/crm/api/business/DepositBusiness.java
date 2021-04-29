ce4dd811557fa24af04790f2540268c37a170ee7
GregoriSimei <gregorisimei@gmail.com>
2021-04-29 02:54:58 -0300

    Route to Create Deposit fixed

--- SOgestao/src/main/java/com/crm/api/business/DepositBusiness.java
+++ SOgestao/src/main/java/com/crm/api/business/DepositBusiness.java
@@ -3,33 +3,31 @@ package com.crm.api.business;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.stereotype.Service;
 
 import com.crm.api.models.Branch;
-import com.crm.api.models.Company;
 import com.crm.api.models.Deposit;
 import com.crm.api.repositories.BranchRepository;
 import com.crm.api.repositories.DepositRepository;
 
+@Service
+@Configurable
 public class DepositBusiness {
+	
 	@Autowired
 	private DepositRepository depositRepository;
+	
 	@Autowired
 	private BranchRepository branchRepository;
-	public Branch saveDeposit(List<Deposit> deps, long idfilial) {
+	
+	public Branch saveDeposit(Deposit deposit, long idfilial) {
 		
 		Branch branch = branchRepository.findById(idfilial);
-		for (Deposit dep : branch.getDeposits()) {
-			if(!deps.contains(dep.getId())) {	// se o dep nao estiver cadastrado na filial
-				branch.setDeposits(deps);
-				branchRepository.save(branch);
-			}
-			else {
-				System.out.println("deposito já cadastrado nessa filial");
-			}
-		}
+		branch.setDeposit(deposit);
+		depositRepository.save(deposit);
+		branchRepository.save(branch);
 		
 		return branch;
 	}
-	
-
 }
