30be007f498ce13d61b44c34a112482757154d15
NeresDu <eduardoneress12@gmail.com>
2021-04-30 21:22:37 -0300

    creating branchcontroller

--- SOgestao/src/main/java/com/crm/api/controllers/BranchController.java
+++ SOgestao/src/main/java/com/crm/api/controllers/BranchController.java
@@ -1,5 +1,45 @@
 package com.crm.api.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.crm.api.models.Branch;
+import com.crm.api.models.Company;
+import com.crm.api.repositories.AddressRepository;
+import com.crm.api.repositories.BranchRepository;
+import com.crm.api.service.BranchService;
+import com.crm.api.service.CompanyService;
+
+@RestController
+@RequestMapping("/company")
 public class BranchController {
+	
+	@Autowired
+	private BranchService branchService;
+	
+	@PostMapping(consumes = "application/json", produces = "application/json")
+	public Company putCompany(@RequestBody Branch branch, @RequestParam(name = "id") long id ) {
+		Company response = branchService.setBranchesById(branch, id);
+		
+		return response;
+	}
+	
+	@PutMapping(consumes = "application/json", produces = "application/json")
+	public Branch updateBranch(@RequestBody Branch branch) {
+		Branch response = branchService.updateBranch(branch);
+		return response;
+	}
+	
+	@DeleteMapping(consumes = "application/json", produces = "application/json")
+	public Branch deleteBranch(@RequestBody Branch branch) {
+		Branch response = branchService.deleteBranch(branch);
+		return response;
+	}
 
 }
