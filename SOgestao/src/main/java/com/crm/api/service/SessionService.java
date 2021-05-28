1149b1de8628ad5e6fb1f13197e0eb208ef8cfe2
GregoriSimei <gregorisimei@gmail.com>
2021-05-28 01:44:02 -0300

    fixing session service to create a session on database

--- SOgestao/src/main/java/com/crm/api/service/SessionService.java
+++ SOgestao/src/main/java/com/crm/api/service/SessionService.java
@@ -2,19 +2,25 @@ package com.crm.api.service;
 
 import java.util.Date;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.stereotype.Service;
 
 import com.crm.api.models.Session;
+import com.crm.api.repositories.SessionRepository;
 
 @Service
 @Configurable
 public class SessionService {
+	
+	@Autowired
+	private SessionRepository sessionRepository;
 
 	public Session generateSession() {
 		Session session = new Session();
 		session.setDate(new Date());
 		session.setStatus("active");
+		session = this.sessionRepository.save(session);
 		
 		return session;
 	}
