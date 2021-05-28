949d6b9fa92cd77b240909cfe1757bff07746a83
GregoriSimei <gregorisimei@gmail.com>
2021-05-28 00:40:51 -0300

    Repositories created

---
+++ SOgestao/src/main/java/com/crm/api/repositories/ItemSaleRepository.java
@@ -0,0 +1,9 @@
+package com.crm.api.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.crm.api.models.ItemSale;
+
+public interface ItemSaleRepository extends CrudRepository<ItemSale, Long>{
+
+}
