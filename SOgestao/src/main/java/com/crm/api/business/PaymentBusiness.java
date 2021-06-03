76b866badfdcdbb5d9ea5f48ad86d0cfcd658f42
GregoriSimei <gregorisimei@gmail.com>
2021-06-02 22:34:45 -0300

    Controller created and the base payment structure

---
+++ SOgestao/src/main/java/com/crm/api/business/PaymentBusiness.java
@@ -0,0 +1,29 @@
+package com.crm.api.business;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.stereotype.Service;
+
+import com.crm.api.models.Payment;
+import com.crm.api.models.Sale;
+import com.crm.api.service.PaymentService;
+import com.crm.api.service.SaleService;
+import com.crm.api.service.SessionService;
+
+@Service
+@Configurable
+public class PaymentBusiness {
+
+	@Autowired
+	private PaymentService paymentService;
+	
+	@Autowired
+	private SaleService saleService;
+	
+	@Autowired
+	private SessionService sessionService;
+	
+	public Sale paidOut(Payment payment) {
+		return null;
+	}
+}
