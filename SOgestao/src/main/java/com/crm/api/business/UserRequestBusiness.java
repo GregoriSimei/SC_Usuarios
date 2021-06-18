19bbb98178e6ee76112b17756d6832c8874e1c53
GregoriSimei <gregorisimei@gmail.com>
2021-06-17 21:53:34 -0300

    create an user after to approve a request

--- SOgestao/src/main/java/com/crm/api/business/UserRequestBusiness.java
+++ SOgestao/src/main/java/com/crm/api/business/UserRequestBusiness.java
@@ -1,15 +1,19 @@
 package com.crm.api.business;
 
+import java.util.Date;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.stereotype.Service;
 
 import com.crm.api.models.Address;
 import com.crm.api.models.Person;
+import com.crm.api.models.User;
 import com.crm.api.models.UserRequest;
 import com.crm.api.service.AddressService;
 import com.crm.api.service.PersonService;
 import com.crm.api.service.UserRequestService;
+import com.crm.api.service.UserService;
 
 @Service
 @Configurable
@@ -22,10 +26,13 @@ public class UserRequestBusiness {
 	private PersonService personService;
 	
 	@Autowired
+	private UserService userService;
+	
+	@Autowired
 	private UserRequestService userRequestService;
 
 	public UserRequest createRequest(UserRequest request) {
-		boolean validation = this.checkRequest(request);
+		boolean validation = this.checkAllFilds(request);
 		
 		request = validation? 
 				this.saveRequest(request):
@@ -33,28 +40,81 @@ public class UserRequestBusiness {
 		
 		return request;
 	}
+
+	public UserRequest updateRequest(UserRequest request) {
+		boolean validation = this.checkAllFilds(request);
+		boolean checkStatus = this.checkStatus(request);
+		request = validation && checkStatus ? 
+				this.approvalOrNot(request):
+				null;
+
+		return request;
+	}
 	
-	private boolean checkRequest(UserRequest request) {
-		boolean requestValidation  = this.userRequestService.checkFields(request);
-		boolean personValidation = this.checkPerson(request);
-		boolean addressValidation = this.checkAddress(request);
+	private UserRequest approvalOrNot(UserRequest request) {
+		String status = request.getStatus();
+		if(status.contentEquals("Approved")) {
+			User user = this.generateUser(request);
+			user = this.userService.save(user);
+			request.getPerson().setUser(user);
+			request = this.userRequestService.requestApproved(request);
+		}
+		else if(status.contentEquals("Not Approved")) {
+			request = this.userRequestService.requestNotApproved(request);
+		}
+		return request;
+	}
+
+	private User generateUser(UserRequest request) {
+		Person person = request.getPerson();
+		person = this.personService.getPerson(person);
+		
+		String name = person.getName();
+		Date birth = person.getBirth();
+		
+		return this.userService.createUser(name, birth);
+	}
+
+	private boolean checkAllFilds(UserRequest request) {
+		boolean requestValidation  = this.checkRequestFilds(request);
+		boolean personValidation = this.checkPersonFilds(request);
+		boolean addressValidation = this.checkAddressFilds(request);
 		
 		return requestValidation &&
 				personValidation &&
 				addressValidation;
 	}
 	
-	private boolean checkPerson(UserRequest request) {
+	private boolean checkRequestFilds(UserRequest request) {
+		return this.userRequestService
+				.checkFields(request);
+	}
+	
+	private boolean checkPersonFilds(UserRequest request) {
 		Person person = request.getPerson();
-		boolean personValidation = this.personService.checkFields(person);
+		boolean personValidation = person != null ?
+					this.personService.checkFields(person):
+					false;
 		return personValidation;
 	}
 
-	private boolean checkAddress(UserRequest request) {
+	private boolean checkAddressFilds(UserRequest request) {
 		Person person = request.getPerson();
-		Address address = person.getAddress();
-		boolean addressValidation = this.addressService.checkFields(address);
+		
+		Address address = person != null ? 
+					person.getAddress():
+					null;
+		
+		boolean addressValidation = address != null ?
+					this.addressService.checkFields(address):
+					false;
+		
 		return addressValidation;
+	}
+	
+	private boolean checkStatus(UserRequest request) {
+		String status = request.getStatus();
+		return this.userRequestService.checkStatus(status);
 	}
 	
 	private UserRequest saveRequest(UserRequest request) {
@@ -72,4 +132,5 @@ public class UserRequestBusiness {
 	private Person savePerson(Person person) {
 		return this.personService.save(person);
 	}
+
 }
