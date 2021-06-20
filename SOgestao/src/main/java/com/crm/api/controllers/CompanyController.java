6744f97b5c0e863aa9b07b60d1420348b3fd2cc4
GregoriSimei <gregorisimei@gmail.com>
2021-06-20 18:15:17 -0300

    REST Company

--- SOgestao/src/main/java/com/crm/api/controllers/CompanyController.java
+++ SOgestao/src/main/java/com/crm/api/controllers/CompanyController.java
@@ -1,9 +1,13 @@
 package com.crm.api.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.crm.api.models.Company;
@@ -11,16 +15,29 @@ import com.crm.api.service.CompanyService;
 
 @RestController
 @RequestMapping("/company")
-public class CompanyController {
-	
-	
+public class CompanyController{
+
 	@Autowired
-	private CompanyService companyBusiness;
+	private CompanyService companyService;
 	
 	@PostMapping(consumes = "application/json", produces = "application/json")
-	public Company postCompany(@RequestBody Company company) {
-		Company response = companyBusiness.saveCompany(company);
-		return response;
+	public Company create(@RequestBody Company company) {
+		return this.companyService.save(company);
+	}
+
+	@PutMapping(consumes = "application/json", produces = "application/json")
+	public Company update(@RequestBody Company company) {
+		return this.companyService.save(company);
+	}
+	
+	@GetMapping(produces = "application/json")
+	public Company getById(@RequestParam(name = "id") Long id) {
+		return this.companyService.findById(id);
+	}
+
+	@DeleteMapping(consumes = "application/json", produces = "application/json")
+	public boolean delete(@RequestBody Company object) {
+		return false;
 	}
 	
 }
