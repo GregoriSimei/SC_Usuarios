46785858a50b8b8765ba3e0c002aaebd968e6f92
GregoriSimei <gregorisimei@gmail.com>
2021-06-20 18:16:27 -0300

    Removed Invoice

--- SOgestao/src/main/java/com/crm/api/repositories/InvoiceRepository.java
+++
@@ -1,9 +0,0 @@
-package com.crm.api.repositories;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.crm.api.models.Invoice;
-
-public interface InvoiceRepository extends CrudRepository<Invoice, Long>{
-
-}
