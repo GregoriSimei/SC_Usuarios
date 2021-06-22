5e6ba945b2a74c8743c9ee3bb75f85dd3bf97903
GregoriSimei <gregorisimei@gmail.com>
2021-06-22 20:49:52 -0300

    Route to Create an item

--- SOgestao/src/main/java/com/crm/api/controllers/ItemController.java
+++ SOgestao/src/main/java/com/crm/api/controllers/ItemController.java
@@ -1,47 +1,46 @@
 package com.crm.api.controllers;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.crm.api.business.ItemBusiness;
 import com.crm.api.models.Item;
-import com.crm.api.service.ItemService;
 
 @RestController
 @RequestMapping("/item")
 public class ItemController {
 	
 	@Autowired
-	private ItemService itemService;
-
+	private ItemBusiness itemBusiness;
+	
 	@PostMapping(consumes = "application/json", produces = "application/json")
-	public Item postItemInDeposit(@RequestBody Item item,@RequestParam("id") long id) {
-		item = this.itemService.save(item ,id);
-		return item;
+	public Item create(@RequestBody Item item,@RequestParam("id") Long depositId) {
+		return this.itemBusiness.createItem(item, depositId);
 	}
-	
-	@GetMapping(value = "/branch" ,produces = "application/json")
-	public List<Item> getAllbyBranch(@RequestParam("id") long id){
-		List<Item> items = this.itemService.getByBranchId(id);
-		return items;
+
+	@PutMapping(consumes = "application/json", produces = "application/json")
+	public Item update(Item object) {
+		// TODO Auto-generated method stub
+		return null;
 	}
-	
-	@GetMapping(value = "/deposit", produces = "application/json")
-	public List<Item> getAllbyDeposit(@RequestParam("id") long id){
-		List<Item> items = this.itemService.getByDepositId(id);
-		return items;
-	}
-	
+
 	@GetMapping(produces = "application/json")
-	public Item getItemById(@RequestParam("id") long id) {
-		Item item = this.itemService.getItemById(id);
-		return item;
+	public Item getById(Item id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@DeleteMapping(consumes = "application/json", produces = "application/json")
+	public boolean delete(Item object) {
+		// TODO Auto-generated method stub
+		return false;
 	}
 	
 }
